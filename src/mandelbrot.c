e62086bc38aef686ced12536bcda005df31e0f13
Guilherme Salustiano <guissalustiano@gmail.com>
2023-09-01 13:51:02 -0300

    add comments to mandebrot.c

--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -7,18 +7,27 @@
 
 const size_t MAX_ITERATIONS = 200;
 
+/*
+ * Armazena as cores de cada pixel da imagem
+ */
 typedef struct {
   uint8_t red;
   uint8_t green;
   uint8_t blue;
 } pixel_t;
 
+/*
+ * Armazena as informações da imagem, com um array de pixels que devera ser alocado na criação
+ */
 typedef struct {
   pixel_t *pixels;
   const size_t width;
   const size_t height;
 } image_t;
 
+/*
+ * Area do conjunto de mandelbrot que será gerada
+ */
 typedef struct {
   double real_min;
   double real_max;
@@ -26,6 +35,10 @@ typedef struct {
   double imag_max;
 } frame_t;
 
+/*
+ * Paleta de cores
+ * Será dada cores diferentes para o numero de iterações que demorou para o ponto divergir
+ */
 const size_t PALLET_SIZE = 17;
 pixel_t pallet[] = {
     {66, 30, 15},    {25, 7, 26},     {9, 1, 47},      {4, 4, 73},
@@ -35,10 +48,17 @@ pixel_t pallet[] = {
     {16, 16, 16},
 };
 
+/*
+ * Função usada para calcular o proximo ponto da sequencia
+ */
 double complex fc(const double complex z, const double complex c) {
   return z * z + c;
 }
 
+/*
+ * Iteramos a partir de um ponto multiplas vezes com a função fc
+ * Se o ponto divergir, retornamos o numero de iterações que demorou para divergir
+ */
 size_t iterate_point(const double complex z0) {
   double complex z = z0;
   for (size_t i = 0; i < MAX_ITERATIONS; i++) {
@@ -51,34 +71,55 @@ size_t iterate_point(const double complex z0) {
   return MAX_ITERATIONS;
 }
 
+/*
+ * Gera a cor do pixel a partir do numero de iterações que demorou para divergir
+ */
 pixel_t iteration2pixel(size_t iterations) {
+  // Se o ponto "não divergiu", ele é preto
   if (iterations >= MAX_ITERATIONS) {
     return pallet[PALLET_SIZE - 1];
   }
+
+  // Se não é alguma outra cor da paleta
   return pallet[iterations % PALLET_SIZE];
 }
 
+/*
+ * A partir do frame (com o retangulo que deve ser gerado)
+ * calcula o passo de cada pixel.
+ * Então percorre os pixels aplicando a função multiplas vezes e observando quando diverge.
+ */
 void create_mandelbrot(const frame_t *frame, image_t *image) {
+  // Calcula o espaço entre cada pixel
   double real_step = (frame->real_max - frame->real_min) / image->width;
   double imag_step = (frame->imag_max - frame->imag_min) / image->height;
 
+  // Ultiliza o OpemMP para paralelizar o for, ou seja, cada linha do programa é calculada em uma thread diferente
   #pragma omp parallel for
   for (int y = 0; y < image->height; y++) {
+    // A partir do step e do ponto inicial, calcula o valor da parte imaginaria referente ao ponto (https://pt.wikipedia.org/wiki/Interpola%C3%A7%C3%A3o_linear)
     double imag = frame->imag_min + y * imag_step;
 
+    // Garante que o ponto seja 0 caso esteja muito proximo de 0
     if (fabs(imag) < imag_step / 2) {
       imag = 0.0;
     };
 
     for (int x = 0; x < image->width; x++) {
+      // Calcula a parte real referente ao ponto
       double real = frame->real_min + x * real_step;
 
+      // Itera multiplas vezes sobre o ponto para ver quado diverge
       int iteration = iterate_point(real + imag * I);
+
+      // Prenche a imagem com a cor referente ao numero de iterações
       image->pixels[x + y * image->width] = iteration2pixel(iteration);
     };
   };
 };
 
+
+// Escreve o arquivo usando o formato ppm P6 (binario)
 void write_image(const image_t *image, const char *filename) {
   FILE *file = fopen(filename, "wb");
 
@@ -93,6 +134,7 @@ void write_image(const image_t *image, const char *filename) {
   fclose(file);
 }
 
+// Parceia os parametros e chama a função principal
 int main(int argc, char *argv[]) {
   if(argc < 6){
         printf("usage: ./mandelbrot real_min real_max imag_min imag_max image_width\n");
